81e00e1431952dab4b08df1b5a8c20e3f99ec6e5
chenxu <2475232977@qq.com>
2026-04-19 13:24:54 +0800

    日志函数完成

--- src/log.c
+++ src/log.c
@@ -1,5 +1,35 @@
 #include "log.h"
 
+/**
+ * @name       errorPrint
+ * @brief      Printed error logs 打印错误的日志
+ * @param      
+ *             @str print character 要打印的字符
+ * @retval     None
+ * @date       2026/04/19
+ * @version    1.0
+ * @note       
+ */
+void errorPrint(const char* str)
+{
+    if (!g_error_log)
+    {
+        printf("err_log false"); 
+        return;
+    }
+    printf("errlog: %s",str);
+}
+
+/**
+ * @name       LogPrint
+ * @brief      Printed console display logs and text logs 打印的控制台显示日志与文本的日志
+ * @param      
+ *             @str print character 要打印的字符
+ * @retval     None
+ * @date       2026/04/19
+ * @version    1.0
+ * @note       
+ */
 void LogPrint(const char* str)
 {
     if (!g_log_enabled)
@@ -15,6 +45,16 @@ void LogPrint(const char* str)
     }
 }
 
+/**
+ * @name       fileLogPrint
+ * @brief      Printed text logs 打印文本的日志
+ * @param      
+ *             @Filestr print character 要打印的字符
+ * @retval     None
+ * @date       2026/04/19
+ * @version    1.0
+ * @note       
+ */
 bool fileLogPrint(const char* Filestr)
 {
     FILE *file = fopen("log.txt", "ab");
